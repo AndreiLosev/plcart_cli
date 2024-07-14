f37346e7bfba863b1150376518ee9ee076b57823
Andrei Losev <andrew.l@spur-i-t.com>
2024-07-14 12:50:15 +0300

    add request id to protocol

--- bin/plcart_cli.dart
+++ bin/plcart_cli.dart
@@ -10,20 +10,20 @@ void main(List<String> arguments) async {
   await client.connect();
   listen(client, c);
 
-  client.write(ClientCommand(CommandKind.getRegisteredTasks, null));
-  client.write(ClientCommand(CommandKind.getRegisteredEvents, null));
+  client.write(ClientCommand(CommandKind.getRegisteredTasks, null), 1);
+  client.write(ClientCommand(CommandKind.getRegisteredEvents, null), 2);
+  client.write(ClientCommand(CommandKind.runEvent,
+      RunEventPayload("TestEvent", [10000000000], {'y': 0.00000000009})), 3);
   client.write(ClientCommand(
-      CommandKind.runEvent, RunEventPayload("TestEvent", [10000000000], {'y': 0.00000000009})));
-  client.write(ClientCommand(
-      CommandKind.subscribeTask, SimplePayload({'value': "TestTask1"})));
+      CommandKind.subscribeTask, SimplePayload({'value': "TestTask1"})), 4);
   await Future.delayed(const Duration(seconds: 5));
   client.write(ClientCommand(
-      CommandKind.subscribeTask, SimplePayload({'value': "TestTask2"})));
+      CommandKind.subscribeTask, SimplePayload({'value': "TestTask2"})), 5);
   client.write(ClientCommand(
-      CommandKind.unsubscribeTask, SimplePayload({"value": "TestTask1"})));
+      CommandKind.unsubscribeTask, SimplePayload({"value": "TestTask1"})), 6);
   await Future.delayed(const Duration(seconds: 5));
   client.write(ClientCommand(
-      CommandKind.unsubscribeTask, SimplePayload({'value': "TestTask2"})));
+      CommandKind.unsubscribeTask, SimplePayload({'value': "TestTask2"})), 7);
 
   await client.disconnect();
 }
@@ -31,6 +31,6 @@ void main(List<String> arguments) async {
 Future<void> listen(Client client, Stopwatch t) async {
   while (client.isConnected()) {
     final res = await client.read();
-    print({res.responseStatus: res.message, 't': t.elapsed});
+    print({res.responseStatus: res.message, 't': t.elapsed, 'id': res.id});
   }
 }
